61bd53b180f301ea685bcb2d5b2020b4c05867b9
Amir Ali <uiuu3014@gmail.com>
2024-11-06 11:36:47 +0330

    Update text2excel.py
    Added keyboard shortcuts for context items
    Improved menu color

--- text2excel.py
+++ text2excel.py
@@ -6,9 +6,19 @@ from os.path import isfile
 from tkinter.simpledialog import askstring
 
 APP_TITLE = 'Text2Excel'
+
 LOG_DEFAULT = 'log ...'
 
-show_error = lambda err : messagebox.showerror(title=APP_TITLE, message=err)
+menu_color_args = {'activebackground' : '#00c8ff', 'activeforeground' : 'black'}
+
+def ask():
+    return askstring(title=APP_TITLE,prompt='Enter the pattern:')
+
+def show_error(err):
+    messagebox.showerror(title=APP_TITLE, message=err)
+
+def delete_all(event=None):
+    patterns_list.delete(0,'end')
 
 def browse_files(widget):
     file_path = filedialog.askopenfilename(title='Browse')
@@ -16,7 +26,7 @@ def browse_files(widget):
         widget.delete(0,'end')
         widget.insert('end',file_path)
 
-def copy_log():
+def copy_log(event=None):
     window.clipboard_clear()
     try:
         data = log_text.selection_get()
@@ -24,11 +34,75 @@ def copy_log():
         data = log_text.get('1.0','end')
     window.clipboard_append(data)
 
-def clear_log():
+def clear_log(event=None):
     log_text.config(state='normal')
     log_text.delete('1.0','end')
     log_text.insert('end',LOG_DEFAULT)
     log_text.config(state='disabled')
+
+def copy_pattern(event=None,all=False):
+    window.clipboard_clear()
+    if all:
+        window.clipboard_append('\n'.join(patterns_list.get(0,'end')))
+    else:
+        selected = patterns_list.curselection()
+        if selected:
+            window.clipboard_append('\n'.join(patterns_list.get(selected[0],selected[-1])))
+
+def import_from_file(event=None):
+    try:
+        file_path = filedialog.askopenfilename(title='Import')
+        if file_path:
+            with open(file_path) as f:
+                for i in f.read().strip().splitlines():
+                    patterns_list.insert('end', i)
+    except Exception as err:
+         show_error(err)
+
+def export_to_file(event=None):
+    try:
+        file_path = filedialog.asksaveasfilename(title='Export')
+        if file_path:
+            with open(file_path, 'w') as f:
+                for i in patterns_list.get(0,'end'):
+                    f.write(i + '\n')
+    except Exception as err:
+        show_error(err)
+
+def delete_selected(event=None):
+    selected = patterns_list.curselection()
+    if selected:
+        patterns_list.delete(selected[0],selected[-1])
+
+def edit_selected(event=None):
+    index = patterns_list.curselection()
+    if len(index) == 1:
+        value = patterns_list.get(index)
+        new_value = askstring(title=APP_TITLE, prompt='Enter the pattern: ', initialvalue=value)
+        if new_value:
+            patterns_list.delete(index)
+            patterns_list.insert(index, new_value)
+    
+def add_pattern(event=None):
+    new_pattern = ask()
+    patterns_list.insert('end',new_pattern)
+
+def insert_pattern(event=None):
+    selected = patterns_list.curselection()
+    if len(selected) == 1:
+        new_pattern = ask()
+        patterns_list.insert(selected[0],new_pattern)
+
+
+def set_patterns(patterns):
+    l=[]
+    for x,y in enumerate(patterns, 1):
+        if not '?P<item>' in y:
+            y = '(?P<item>' + y + ')'
+        if col_var.get():
+            x = get_column_letter(x)
+        l.append((str(x),y))
+    return l
 
 def find_max(wb, index, sheet_name):
     if col_var.get():
@@ -43,64 +117,6 @@ def find_max(wb, index, sheet_name):
             if i[0].value is not None:
                 col = i[0].column
         return col
-
-def copy_pattern(all=False):
-    window.clipboard_clear()
-    if all:
-        window.clipboard_append('\n'.join(patterns_list.get(0,'end')))
-    else:
-        selected = patterns_list.curselection()
-        window.clipboard_append('\n'.join(patterns_list.get(selected[0],selected[-1])))
-
-def import_from_file():
-    try:
-        file_path = filedialog.askopenfilename(title='Import')
-        if file_path:
-            with open(file_path) as f:
-                for i in f.read().strip().splitlines():
-                    patterns_list.insert('end', i)
-    except Exception as err:
-         show_error(err)
-
-def export_to_file():
-    try:
-        file_path = filedialog.asksaveasfilename(title='Export')
-        if file_path:
-            with open(file_path, 'w') as f:
-                for i in patterns_list.get(0,'end'):
-                    f.write(i + '\n')
-    except Exception as err:
-        show_error(err)
-
-def delete_selected():
-    selected = patterns_list.curselection()
-    patterns_list.delete(selected[0],selected[-1])
-
-def edit_selected():
-    index = patterns_list.curselection()
-    value = patterns_list.get(index)
-    new_value = askstring(title=APP_TITLE, prompt='Enter the pattern: ', initialvalue=value)
-    if new_value:
-        patterns_list.delete(index)
-        patterns_list.insert(index, new_value)
-    
-def add_pattern(insert=False):
-    new_pattern = askstring(title=APP_TITLE, prompt='Enter the pattern: ')
-    if insert:
-        new_index = patterns_list.curselection()[0]
-        patterns_list.insert(new_index,new_pattern)
-    else:
-        patterns_list.insert('end',new_pattern)
-
-def set_patterns(patterns):
-    l=[]
-    for x,y in enumerate(patterns, 1):
-        if not '?P<item>' in y:
-            y = '(?P<item>' + y + ')'
-        if col_var.get():
-            x = get_column_letter(x)
-        l.append((str(x),y))
-    return l
 
 def create_excel_file(output_file,input_file,sheet_name, patterns):
         try:
@@ -213,28 +229,28 @@ def show_entry_menu(menu,event,app=False):
         menu.tk_popup(event.x_root,event.y_root)
 
 def create_patterns_menu():
-     menu = tk.Menu(tearoff=False)
-     menu.add_command(label='Add Pattern', command=add_pattern)
-     menu.add_command(label='Insert Pattern',command=lambda : add_pattern(True))
+     menu = tk.Menu(tearoff=False,**menu_color_args)
+     menu.add_command(label='Add Pattern', command=add_pattern,accelerator='Ctrl+A')
+     menu.add_command(label='Insert Pattern',command=insert_pattern,accelerator='Ctrl+I')
      menu.add_separator()
-     menu.add_command(label='Edit selected', command=edit_selected)
-     menu.add_command(label='Delete selected', command=delete_selected)
-     menu.add_command(label='Copy selected', command=lambda : copy_pattern())
-     menu.add_command(label='Delete All', command=lambda : patterns_list.delete(0,'end'))
-     menu.add_command(label='Copy All', command=lambda : copy_pattern(True))
+     menu.add_command(label='Edit selected', command=edit_selected,accelerator='F2')
+     menu.add_command(label='Delete selected', command=delete_selected,accelerator='Delete')
+     menu.add_command(label='Copy selected', command=copy_pattern,accelerator='Ctrl+C')
+     menu.add_command(label='Delete All', command=delete_all,accelerator='Ctrl+Shift+D')
+     menu.add_command(label='Copy All', command=lambda : copy_pattern(all=True),accelerator='Ctrl+Shift+C')
      menu.add_separator()
-     menu.add_command(label='Import from file', command=import_from_file)
-     menu.add_command(label='Export to file', command=export_to_file)
+     menu.add_command(label='Import from file', command=import_from_file,accelerator='Ctrl+Shift+I')
+     menu.add_command(label='Export to file', command=export_to_file,accelerator='Ctrl+E')
      return menu
 
 def create_log_menu():
-    menu = tk.Menu(tearoff=False)
-    menu.add_command(label='Copy log',command=copy_log)
-    menu.add_command(label='Clear log',command=clear_log)
+    menu = tk.Menu(tearoff=False,**menu_color_args)
+    menu.add_command(label='Copy log',command=copy_log,accelerator='Ctrl+C')
+    menu.add_command(label='Clear log',command=clear_log,accelerator='Ctrl+D')
     return menu
 
 def create_entry_menu(widget,is_file_entry=True):
-    menu = tk.Menu(tearoff=False)
+    menu = tk.Menu(tearoff=False,**menu_color_args)
     menu.add_command(label='Select All', accelerator='Ctrl+A',command=lambda : widget.select_range(0,'end'))
     menu.add_command(label='Copy', accelerator='Ctrl+C',command=lambda : widget.event_generate('<<Copy>>'))
     menu.add_command(label='Paste', accelerator='Ctrl+V',command=lambda : widget.event_generate('<<Paste>>'))
@@ -242,29 +258,33 @@ def create_entry_menu(widget,is_file_entry=True):
     menu.add_separator()
     menu.add_command(label='Clear',accelerator='Ctrl+Shift+C',command=lambda : widget.delete(0,'end'))
     if is_file_entry:
-        menu.add_command(label='Browse',command=lambda : browse_files(widget))
+        menu.add_command(label='Browse',command=lambda : browse_files(widget),accelerator='Ctrl+B')
     return menu
 
 window =tk.Tk()
 window.title(APP_TITLE)
 window.resizable(False,False)
+
 main_frm = tk.Frame()
+
 frm = tk.Frame(main_frm,borderwidth=10)
+frm.pack()
+
 input_file_lbl = tk.Label(frm,text='Input file:')
-input_file_entry = ttk.Entry(frm,width=30)
 output_file_lbl = tk.Label(frm,text='Output file:')
-output_file_entry = ttk.Entry(frm,width=30)
 sheet_name_lbl = tk.Label(frm,text='Sheet name:')
+
+input_file_entry = ttk.Entry(frm,width=30)
+output_file_entry = ttk.Entry(frm,width=30)
 sheet_name_entry = ttk.Entry(frm,width=10)
 
 input_file_lbl.grid(row=2,column=0,sticky='w')
-input_file_entry.grid(row=3,column=0,sticky='w')
 output_file_lbl.grid(row=2, column=3,sticky='w')
-output_file_entry.grid(row=3,column=3,sticky='w')
 sheet_name_lbl.grid(row=4,column=3,sticky='w')
-sheet_name_entry.grid(row=5,column=3,sticky='w')
 
-frm.pack()
+input_file_entry.grid(row=3,column=0,sticky='w')
+output_file_entry.grid(row=3,column=3,sticky='w')
+sheet_name_entry.grid(row=5,column=3,sticky='w')
 
 log_frm = tk.Frame(main_frm)
 yscroll_log = tk.Scrollbar(log_frm)
@@ -280,6 +300,7 @@ log_text.grid(row=0,column=0)
 xscroll_log.grid(row=1,column=0, pady=(0,10), sticky='we')
 yscroll_log.grid(row=0,column=1, sticky='ns')
 log_frm.pack()
+
 main_frm.grid(row=0,column=0, rowspan=3)
 
 exact_var = tk.IntVar()
@@ -308,7 +329,6 @@ xscroll_pl.grid(row=2,column=0, sticky='we')
 yscroll_pl.grid(row=1,column=1,sticky='ns')
 patterns_list_frm.grid(row=2,column=1, sticky='s')
 
-
 input_file_menu = create_entry_menu(input_file_entry)
 output_file_menu = create_entry_menu(output_file_entry)
 sheet_name_menu = create_entry_menu(sheet_name_entry,False)
@@ -332,9 +352,25 @@ sheet_name_entry.bind('<App>',lambda event : show_entry_menu(sheet_name_menu,eve
 
 window.bind_class('TEntry','<Control-C>',lambda event : event.widget.delete(0,'end'))
 
+log_text.bind('<Control-c>',copy_log)
+log_text.bind('<Control-d>',clear_log)
+
+input_file_entry.bind('<Control-b>',lambda event  : browse_files(event.widget))
+output_file_entry.bind('<Control-b>',lambda event : browse_files(event.widget))
+
 input_file_entry.focus_set()
 
 log_text.bind('<FocusOut>',lambda event : log_text.tag_remove('sel','1.0','end'))
+
+patterns_list.bind('<Control-a>',add_pattern)
+patterns_list.bind('<Control-i>',insert_pattern)
+patterns_list.bind('<F2>',edit_selected)
+patterns_list.bind('<Delete>',delete_selected)
+patterns_list.bind('<Control-c>',copy_pattern)
+patterns_list.bind('<Control-D>',delete_all)
+patterns_list.bind('<Control-C>',lambda event : copy_pattern(all=True))
+patterns_list.bind('<Control-I>',import_from_file)
+patterns_list.bind('<Control-e>',export_to_file)
 
 btn_convert = tk.Button(frm,text='convert',width=10,height=5,background='#0080e5',
                         command=lambda : create_excel_file(output_file_entry.get(), input_file_entry.get(), sheet_name_entry.get(), 
